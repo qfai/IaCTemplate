693a2c06bcc104c5bbcbe31c8b33deca2359937a
quke <quke@microsoft.com>
2023-08-22 14:37:27 +0800

    initial commit of Iac Template

---
+++ .azure/aztfexport.sh
@@ -0,0 +1,45 @@
+set -e
+set -x
+
+# Install aztfexport
+
+go install github.com/Azure/aztfexport@latest
+
+# Export resources in aztfexport_stacks.json
+
+while read stack; do
+    name=$(echo "$stack" | jq -r .name)
+    stage=$(echo "$stack" | jq -r .stage)
+
+    echo "Exporting $name to $stage."
+    echo $stack | jq -c ".resource_groups"
+
+    [ ! -e "./$stage/$name" ] || rm -rf "./$stage/$name"
+
+    mkdir "./$stage/$name"
+
+    pushd "./$stage/$name"
+
+    while read rg; do
+        echo "resource group: $rg"
+        sub=$(echo $rg | cut -d "/" -f 3)
+        rg=$(echo $rg | cut -d "/" -f 5)
+        aztfexport resource-group --non-interactive --plain-ui -s "$sub" --append "$rg"
+    done < <(echo "$stack" | jq -r '.resource_groups[]')
+
+    echo "Exported $name to $stage."
+
+    ls
+
+    backend=$(cat ../../.azure/backend.template | sed "s/*/${name}/g")
+    sed -i "s/backend \"local\" {}/$backend/g" ./terraform.aztfexport.tf
+
+    sed -i "s/resourcegroups/resourceGroups/g" ./main.aztfexport.tf
+
+    terraform fmt -write=true
+
+    popd 
+
+done < <(cat ".azure/aztfexport_stack.json" | jq -c '.[]')
+
+rm .azure/aztfexport_stack.json
---
+++ .azure/scale.sh
@@ -0,0 +1,38 @@
+set -e
+set -x
+
+from=$(cat ".azure/scale_stacks.json" | jq -c '.from')
+replaces=()
+fromStage=$(echo $from | jq -r .stage)
+fromName=$(echo $from | jq -r .name)
+srcPath="./$fromStage/$fromName"
+while read replace; do
+    replaces+=($replace)
+done < <(echo $from | jq -r .replaces[])
+
+while read stack; do
+    name=$(echo "$stack" | jq -r .name)
+    stage=$(echo "$stack" | jq -r .stage)
+
+    destPath="./$stage/$name"
+
+    echo "Replicating $srcPath to $destPath."
+
+    [ ! -e $destPath ] || rm -rf $destPath
+
+    cp -r $srcPath $destPath
+
+    pushd $destPath
+    length=${#replaces[@]}
+    for file in *.tf; do
+        for (( i=0; i<${length}; i++ )); do
+            value=$(echo "$stack" | jq -r .replaces[$i])
+            sed -i "s/${replaces[$i]}/$value/g" $file
+        done
+    done
+
+    popd 
+
+done < <(cat ".azure/scale_stacks.json" | jq -c '.to[]')
+
+rm .azure/scale_stacks.json
